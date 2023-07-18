8924495065d3fd3b1aa455aa175ac386e3b56061
ejavi <elimjavier@gmail.com>
2023-07-17 21:36:45 -0500

    start to set up file and vocab loading functionality

--- landing_code.js
+++ landing_code.js
@@ -1,10 +1,10 @@
-function Card(chinese, def, pinyin) {
+function Card(chinese, pinyin, def) {
 
     this.chinese = chinese;
-    this.def = def;
     this.pinyin = pinyin;
+    this.def = def;
 
-}
+    }
 
 var card1 = new Card("中文", "Chinese language", "zhōng wén");
 var card2 = new Card("你好", "Hello", "nǐ hǎo")
@@ -14,7 +14,7 @@ var card3 = new Card("再见", "Goodbye", "zái jián")
 var deck_1 = [card1, card2, card3];
 
 //Library of decks
-var library = [deck_1];
+var library = [];
 
 //Tracking
 var current_card = 0;
@@ -22,8 +22,31 @@ var current_deck = 0;
 var deck_length = deck_1.length;
 
 //Init?
+//  C:\Users\elimj\Desktop\web-learning\vocab
 
+//set up required modules to read through directory
+var fs = require('fs');
+var path = require('path');
+var location = 'C:\\Users\\elimj\\Desktop\\web-learning\\vocab';
 
+// Loop through all the files in the temp directory
+fs.readdir(location, function (err, files) {
+    if (err) {
+      console.error("Could not list the directory.", err);
+      process.exit(1);
+    }
+  
+    files.forEach(function (file, index) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            const text = e.target.result;
+            const array = csvToArray(text);
+            document.write(JSON.stringify(array));
+        };
+        reader.readAsText(file)
+        
+    });
+  });
 
 function nextCard(){
     current_card = (current_card === deck_length - 1) ? 0 : current_card ++;
