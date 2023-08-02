cf6948214e1f17d42a3786834a36e54ef38d6f19
ejavi <elimjavier@gmail.com>
2023-08-01 21:52:57 -0500

    CSV input functionality, loads first card side

--- landing_code.js
+++ landing_code.js
@@ -6,87 +6,40 @@ function Card(chinese, pinyin, def) {
 
     }
 
-var card1 = new Card("中文", "Chinese language", "zhōng wén");
-var card2 = new Card("你好", "Hello", "nǐ hǎo")
-var card3 = new Card("再见", "Goodbye", "zái jián")
-
-
-//Decks
-var deck_1 = [card1, card2, card3];
-
-//Library of decks
+//global deck var
 var library = [];
 
 //Init
 const myForm = document.getElementById("submit_csv");
 const cur_csv = document.getElementById("csvfile");
 
-function csvToArray(str, delimiter = ",") {
-
-    const headers = str.slice(0, str.indexOf("\n")).split(delimiter);
-    const rows = str.slice(str.indexOf("\n") + 1).split("\n");
-
-    // Map the rows  
-    const arr = rows.map(function (row) {
-      const values = row.split(delimiter);
-      const el = headers.reduce(function (object, header, index) {
-        object[header] = values[index];
-        return object;
-      }, {});
-      return el;
-    });
-
-    return arr;
-  }
-
-  myForm.addEventListener("submit", function (e) {
-    e.preventDefault();
-    const input = cur_csv.files[0];
-    console.log(input);//added this
-    const reader = new FileReader();
-
-    reader.onload = function (e) {
-      const text = e.target.result;
-      const data = csvToArray(text);
-      console.log(data);
-      //document.write(JSON.stringify(data));
-    };
-    
-    reader.readAsText(input);
-  });
-
-
-/*
-function upload_csv(){
-
-
-
-
-  
+function pressed() {
     const input = cur_csv.files[0];
     const reader = new FileReader();
-
-    reader.onload = function (e) {
-      const text = e.target.result;
-      const data = csvToArray(text);
-      console.log(data);
-      //document.write(JSON.stringify(data));
-    };   
-    
     reader.readAsText(input);
-    
+    reader.onload = function(event){
+        var data = event.target.result;
+        var rows = data.split("\n");
+        this.deck = rows;
+        load_deck(rows);
+    }
 }
-*/
-function nextCard(){
-    current_card = (current_card === deck_length - 1) ? 0 : current_card ++;
-    
-    curr_chinese = library[current_deck][current_card].chinese;
-    curr_def = library[current_deck][current_card].def;
-    curr_pinyin = library[current_deck][current_card].pinyin;
 
-    console.log(curr_chinese);
-    console.log(curr_def);
-    console.log(curr_pinyin);
-    //use inner HTML here
+function load_deck(cur_deck) {
+    var loaded_deck = cur_deck;
+    loaded_deck.shift(); //remove first line of headers
+    //remove return escape chars
+    for(i in loaded_deck) {
+      let text = loaded_deck[i].substr(0, loaded_deck[i].length - 1);
+      loaded_deck[i] = text;
+    }
 
+    for(i in loaded_deck) {
+      let arr = loaded_deck[i].split(",");
+      library.push(new Card(arr[0], arr[1], arr[2]));
+    }
+
+    document.getElementById("chars").innerHTML = library[0].chinese;
+    document.getElementById("pinyin").innerHTML = "";
+    document.getElementById("def").innerHTML = "";
 }
